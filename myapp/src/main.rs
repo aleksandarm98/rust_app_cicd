f87e7cf69e7fde22e872377055e951e62710b32a
aleksandarm98 <87500272+aleksandarm98@users.noreply.github.com>
2023-09-25 21:41:34 +0200

    Update main.rs

--- myapp/src/main.rs
+++ myapp/src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> std::io::Result<()>
         .service(health_check)
         .service(calculate_power)
     })
-    .bind(("0.0.0.0", 80))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
